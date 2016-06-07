41c82640349ed868c225d354abb6684774540158
wp <747699984@qq.com>
2016-06-07 09:46:08 +0800

    三才签到系统框架搭建

---
+++ application/views/pages/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="utf-8">
+    <title>三才正定签到系统</title>
+</head>
+<body>
+
+<div id="container">
+    《签到系统主页》
+</div>
+
+</body>
+</html>
